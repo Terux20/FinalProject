fe763a2a09a4235f7539be5938dd0059ab31e93d
Terux20 <155570356+Terux20@users.noreply.github.com>
2024-01-20 13:33:05 +0300

    .

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min,decimal max);
+        List<ProductDetailDto> GetProductDetalils();
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthWindContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context= Db tabloları ile proje classlarını bağlamak
+    public class NorthWindContext : DbContext
+    {//veritabanı yolunu bağlama
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+        //veritabanındaki hangi veri sınıfını hangi classa bağlayacağımız bölüm
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders  { get; set; }
+    }
+}
